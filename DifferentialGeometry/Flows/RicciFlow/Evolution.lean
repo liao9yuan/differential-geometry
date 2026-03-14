79e8656a7ab187993c35612566aac5582511bd8e
Ziyang Qin <zq38@cornell.edu>
2026-03-14 15:38:45 -0400

    refactor the structure of flows/ricciflow/ for future complexities

---
+++ DifferentialGeometry/Flows/RicciFlow/Evolution.lean
@@ -0,0 +1,9 @@
+import DifferentialGeometry.Flows.RicciFlow.Basic
+import DifferentialGeometry.Operators.Variation
+
+set_option autoImplicit false
+set_option linter.style.longLine false
+
+/-!
+# Evolution Equations under Ricci Flow
+-/
