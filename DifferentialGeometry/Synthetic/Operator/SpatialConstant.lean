36d040a59c488b5366f32996877579486370b7e7
Ziyang Qin <zq38@cornell.edu>
2026-04-06 22:13:27 -0400

    fill sorry in spatialconstant.lean

--- DifferentialGeometry/Synthetic/Operator/SpatialConstant.lean
+++ DifferentialGeometry/Synthetic/Operator/SpatialConstant.lean
@@ -9,11 +9,6 @@ import DifferentialGeometry.Synthetic.Algebra.Trace
 import Mathlib.Algebra.Module.Basic
 import Mathlib.Algebra.Ring.Basic
 
-set_option autoImplicit false
-set_option linter.style.longLine false
-set_option linter.unusedSectionVars false
-set_option linter.style.emptyLine false
-
 open DifferentialGeometry TensorAlgebra
 
 variable (R V : Type)
@@ -33,7 +28,24 @@ lemma grad_zero (metric : MetricDuality R V) (c : R) [IsSpatialConstant R V c] :
   exact IsSpatialConstant.action_zero X
 
 lemma laplacian_zero (metric : MetricDuality R V) (conn : AbstractAffineConnection R V) [AffineTensorCalculus conn] (c : R) [IsSpatialConstant R V c] : laplacian metric conn c = 0 := by
-  sorry
+  have h1 : grad metric c = 0 := grad_zero metric c
+  have h2 : laplacian metric conn (0:R) = 0 := by
+    have hsub := laplacian_sub metric conn (0:R) (0:R)
+    have hz1 : (0:R) - (0:R) = (0:R) := sub_self (0:R)
+    rw [hz1] at hsub
+    have hz2 : laplacian metric conn (0:R) - laplacian metric conn (0:R) = 0 := sub_self (laplacian metric conn (0:R))
+    rw [hz2] at hsub
+    exact hsub
+  have h3 : grad metric (0:R) = 0 := by
+    have hc0 : IsSpatialConstant R V 0 := ⟨fun X => by rw [action_zero X]⟩
+    exact grad_zero metric 0
+  have h_lap_c : laplacian metric conn c = tensor_eval (metric_trace metric (0: Fin 2) (0: Fin 1) (lower_index metric.toNonDegenerateMetric.toAbstractMetricTensor (0: Fin 1) (covariant_differential metric conn (grad metric c)))) ![] ![] := rfl
+  have h_lap_0 : laplacian metric conn 0 = tensor_eval (metric_trace metric (0: Fin 2) (0: Fin 1) (lower_index metric.toNonDegenerateMetric.toAbstractMetricTensor (0: Fin 1) (covariant_differential metric conn (grad metric 0)))) ![] ![] := rfl
+  rw [h_lap_c]
+  rw [h1]
+  rw [← h3]
+  rw [← h_lap_0]
+  exact h2
 
 lemma grad_smul (metric : MetricDuality R V) (c f : R) [IsSpatialConstant R V c] : grad metric (c * f) = c • grad metric f := by
   apply metric.toNonDegenerateMetric.eq_of_forall_g_eq
@@ -47,4 +59,5 @@ lemma grad_smul (metric : MetricDuality R V) (c f : R) [IsSpatialConstant R V c]
   rw [h1, h2, ← h5, ← h6]
 
 lemma laplacian_const_smul (metric : MetricDuality R V) (conn : AbstractAffineConnection R V) [AffineTensorCalculus conn] (c f : R) [IsSpatialConstant R V c] : laplacian metric conn (c * f) = c * laplacian metric conn f := by
-  sorry
+  apply laplacian_smul
+  exact IsSpatialConstant.action_zero
