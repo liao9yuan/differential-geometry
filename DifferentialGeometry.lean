0f710e03160236774937ab1677ef771a48ee4637
Ziyang Qin <zq38@cornell.edu>
2026-05-28 11:00:16 -0400

    Wire Order2Equivalence into the aggregate; drop premature VariationalFlow import
    Correct the aggregate import list: add the order-2 Sobolev equivalence module and
    remove an import of a module not yet in the tree (it will be re-added with its file).

--- DifferentialGeometry.lean
+++ DifferentialGeometry.lean
@@ -1178,7 +1178,6 @@ import DifferentialGeometry.PDE.RicciFlow.ODE.TimeDependentFlow.Bijective.Unifor
 import DifferentialGeometry.PDE.RicciFlow.ODE.TimeDependentFlow.HartmanAssembly
 import DifferentialGeometry.PDE.RicciFlow.ODE.TimeDependentFlow.ManifoldFlowFamily
 import DifferentialGeometry.PDE.RicciFlow.ODE.TimeDependentFlow.ManifoldIntegralFlow
-import DifferentialGeometry.PDE.RicciFlow.ODE.TimeDependentFlow.VariationalFlow
 import DifferentialGeometry.PDE.RicciFlow.ODE.TimeDependentFlow.PushforwardSmooth
 import DifferentialGeometry.PDE.RicciFlow.IntrinsicSpectral.LiftMetric
 import DifferentialGeometry.PDE.RicciFlow.IntrinsicSpectral.PointwiseDeriv
@@ -1186,6 +1185,7 @@ import DifferentialGeometry.PDE.RicciFlow.DeTurckShortTime
 import DifferentialGeometry.PDE.RicciFlow.DeTurckVFTimeFamily
 import DifferentialGeometry.PDE.RicciFlow.DeTurckSolutionC1
 import DifferentialGeometry.PDE.RicciFlow.ShortTimeExistence
+import DifferentialGeometry.Analysis.Parabolic.TensorSpectral.SobolevScale.Order2Equivalence
 
 -- Public consumption-facing facade
 import DifferentialGeometry.Interface
