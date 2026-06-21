9fc2a6c952ac4f9cbdd0eb1d8afd730f1694075c
Ziyang Qin <zq38@cornell.edu>
2026-06-21 03:09:29 -0400

    Assemble the parabolic forcing-coefficient smoothing from the elliptic forcing-mass coupling and uniform spectral-mass control, reducing to the Nemytskii mass coupling and the interior time-smoothing

---
+++ DifferentialGeometry/Analysis/Spectral/Intrinsic/HeatSemigroup/ForcingFixedPointParabolicSmoothing.lean
@@ -0,0 +1,80 @@
+import DifferentialGeometry.Analysis.Spectral.Intrinsic.DeTurck.SobolevNonlinearityExistence
+import DifferentialGeometry.Analysis.Parabolic.QuasiLinear.TensorMaximalRegularity.SmallTimeSmoothness
+import DifferentialGeometry.Analysis.Parabolic.MaximalRegularity.PerModeL2
+import DifferentialGeometry.Analysis.Spectral.Intrinsic.HeatSemigroup.ParabolicInteriorSmoothing
+
+noncomputable section
+
+open Bundle Manifold MeasureTheory Set Filter
+open scoped Manifold Topology ContDiff ENNReal BigOperators NNReal
+  RealInnerProductSpace InnerProductSpace
+
+namespace DifferentialGeometry
+namespace PDE
+namespace RicciFlow
+namespace IntrinsicSpectral
+
+open DifferentialGeometry.Integral.Measure
+open DifferentialGeometry.Integral.L2
+open DifferentialGeometry.Analysis.Parabolic
+open DifferentialGeometry.Analysis.Parabolic.TensorHeatEquation
+open DifferentialGeometry.Analysis.Parabolic.TimeSobolev
+open DifferentialGeometry.Analysis.Parabolic.MaximalRegularity
+open DifferentialGeometry.Analysis.Parabolic.QuasiLinear
+open DifferentialGeometry.PDE.RicciFlow.IntrinsicSpectral.MetricRealization
+
+variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℝ E]
+  [FiniteDimensional ℝ E] [NeZero (Module.finrank ℝ E)]
+variable {H : Type*} [TopologicalSpace H] {I : ModelWithCorners ℝ E H}
+variable {M : Type*} [TopologicalSpace M] [ChartedSpace H M] [IsManifold I ∞ M]
+  [CompactSpace M] [I.Boundaryless] [BoundarylessManifold I M]
+  [T2Space M] [SigmaCompactSpace M]
+
+theorem deTurckForcing_solFieldMass_forcingMass_couple
+    (g₀ g_bg : SmoothRiemannianMetric I M) (a : ℕ)
+    (ha_super : 2 * Module.finrank ℝ E + 10 ≤ a) (ha_even : Even a)
+    {T : ℝ} (hT : 0 < T) (hT1 : T ≤ 1)
+    (gforce : timeL2 (tensorHs (I := I) (M := M) g₀ 0 2 (a : ℝ)) T)
+    (hforce : gforce =ᵐ[timeMeasure T]
+      (fun t => deTurckSobolevNHa2 (I := I) (M := M) g₀ g_bg a
+        (maxRegDuhamelSolField (I := I) (M := M) (a : ℝ) hT hT1
+          (0 : tensorHs (I := I) (M := M) g₀ 0 2 ((a : ℝ) + 2)) gforce t)))
+    (d : ℝ)
+    (hsol : Summable (solFieldMass (I := I) (M := M) (a := (a : ℝ)) hT.le gforce (d + 1))) :
+    Summable (forcingMass (I := I) (M := M) (a := (a : ℝ)) gforce d) :=
+  sorry
+
+theorem deTurckForcing_ballForcing_admits_smoothDriver
+    (g₀ g_bg : SmoothRiemannianMetric I M) (a : ℕ)
+    (ha_super : 2 * Module.finrank ℝ E + 10 ≤ a) (ha_even : Even a)
+    {T : ℝ} (hT : 0 < T) (hT1 : T ≤ 1)
+    (gforce : timeL2 (tensorHs (I := I) (M := M) g₀ 0 2 (a : ℝ)) T)
+    (hforce : gforce =ᵐ[timeMeasure T]
+      (fun t => deTurckSobolevNHa2 (I := I) (M := M) g₀ g_bg a
+        (maxRegDuhamelSolField (I := I) (M := M) (a : ℝ) hT hT1
+          (0 : tensorHs (I := I) (M := M) g₀ 0 2 ((a : ℝ) + 2)) gforce t)))
+    (huniformMass : ∀ σ : ℝ, ∃ C : ℝ, ∀ t ∈ Set.Icc (0 : ℝ) T,
+      Summable (fun i => tensorSobolevWeight (I := I) (M := M) i σ *
+          (perModeConv (TensorEigenIdx.lambda (I := I) (M := M) i)
+            (fun u => (timeModeCoeff (I := I) (M := M) gforce i) u) t) ^ 2) ∧
+        ∑' i, tensorSobolevWeight (I := I) (M := M) i σ *
+            (perModeConv (TensorEigenIdx.lambda (I := I) (M := M) i)
+              (fun u => (timeModeCoeff (I := I) (M := M) gforce i) u) t) ^ 2 ≤ C) :
+    ∃ d' : ℝ, 0 < d' ∧ d' ≤ T ∧
+      ∃ c : TensorEigenIdx (I := I) (M := M) g₀ 0 2 → ℝ → ℝ,
+      (∀ i, ContDiff ℝ ∞ (c i)) ∧
+      (∀ (j : ℕ) (τ : ℝ), 0 ≤ τ →
+        ∃ B : TensorEigenIdx (I := I) (M := M) g₀ 0 2 → ℝ, Summable B ∧
+          ∀ i, ∀ t ∈ Set.Icc (0 : ℝ) T,
+            tensorSobolevWeight (I := I) (M := M) i τ *
+                (iteratedDeriv j (c i) t) ^ 2 ≤ B i) ∧
+      (∀ i, (timeModeCoeff (I := I) (M := M) gforce i : ℝ → ℝ)
+          =ᵐ[MeasureTheory.volume.restrict (Set.Icc (0 : ℝ) d')] c i) :=
+  sorry
+
+end IntrinsicSpectral
+end RicciFlow
+end PDE
+end DifferentialGeometry
+
+end
