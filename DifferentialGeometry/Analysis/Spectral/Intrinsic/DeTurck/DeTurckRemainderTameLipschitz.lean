ff363045fdcc67d5134294994e950c49877e50eb
Ziyang Qin <zq38@cornell.edu>
2026-07-03 05:04:52 -0400

    Expose public center-point chart evaluations of the realized gram derivative and covariant lower-order terms for the Lie three-arm transfer

--- DifferentialGeometry/Analysis/Spectral/Intrinsic/DeTurck/DeTurckRemainderTameLipschitz.lean
+++ DifferentialGeometry/Analysis/Spectral/Intrinsic/DeTurck/DeTurckRemainderTameLipschitz.lean
@@ -29,6 +29,7 @@ import DifferentialGeometry.Analysis.Parabolic.RicciLinearization.RealizedFamCha
 import DifferentialGeometry.Analysis.Parabolic.DeTurckLinearization.LieDeTurckRemainderOrderSplit
 import DifferentialGeometry.Analysis.Sobolev.TensorHilbert.DeTurckLieKernelL2JetBound
 import DifferentialGeometry.Analysis.Spectral.Tensor.CovGrad.DeTurckLieCoeffAppCcValue
+import DifferentialGeometry.Analysis.Parabolic.RicciLinearization.RealizedGramDerivChartEvaluation
 
 noncomputable section
 
