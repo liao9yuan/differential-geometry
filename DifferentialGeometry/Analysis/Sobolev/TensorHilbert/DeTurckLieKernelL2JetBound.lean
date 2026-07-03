bdf1ef58eaa769e54a6bc7f4c0e242f44c21e4e4
Ziyang Qin <zq38@cornell.edu>
2026-07-03 01:31:09 -0400

    Express the covariant derivative of the DeTurck connection-difference kernel over the background connection via the connection-difference cocycle with explicit quadratic correction terms

--- DifferentialGeometry/Analysis/Sobolev/TensorHilbert/DeTurckLieKernelL2JetBound.lean
+++ DifferentialGeometry/Analysis/Sobolev/TensorHilbert/DeTurckLieKernelL2JetBound.lean
@@ -17,7 +17,8 @@ open DifferentialGeometry.PDE.RicciFlow
 open DifferentialGeometry.Integral.L2
 open DifferentialGeometry.Analysis.Parabolic.TensorSpectral
   (dLaBiContrFib dLaBiContrFib_contMDiff deTurckLieDLbFib deTurckLieDLbFib_contMDiff
-    deTurckLieFib deTurckLieCoeffField deTurckLieCoeffField_toSection)
+    deTurckLieFib deTurckLieCoeffField deTurckLieCoeffField_toSection
+    deTurckLieCovDerivA connDiff_pairing_mdiffAt dLaCovKernel dLaCovKernel_apply_extend)
 
 variable {E : Type*} [NormedAddCommGroup E] [InnerProductSpace ℝ E]
   [FiniteDimensional ℝ E] [NeZero (Module.finrank ℝ E)]
@@ -72,6 +73,190 @@ theorem deTurckLieDLaCoeffField_add_deTurckLieDLbCoeffField
     deTurckLieDLaCoeffField_toSection, deTurckLieDLbCoeffField_toSection,
     deTurckLieCoeffField_toSection]
   rfl
+
+set_option linter.unusedSectionVars false in
+theorem connDiff_cocycle (gA gB gC : SmoothRiemannianMetric I M) (x : M)
+    (u v : TangentSpace I x) :
+    PDE.DeTurck.connDiff (I := I) gA gB x u v + PDE.DeTurck.connDiff (I := I) gB gC x u v =
+      PDE.DeTurck.connDiff (I := I) gA gC x u v := by
+  classical
+  have hσ := smoothExtensionTangent_mdiff (I := I) x u x
+  have hu : smoothExtensionTangent (I := I) x u x = u :=
+    smoothExtensionTangent_eq (I := I) x u
+  rw [← hu]
+  rw [PDE.DeTurck.connDiff_apply (I := I) gA gB hσ v,
+    PDE.DeTurck.connDiff_apply (I := I) gB gC hσ v,
+    PDE.DeTurck.connDiff_apply (I := I) gA gC hσ v]
+  abel
+
+set_option linter.unusedSectionVars false in
+theorem deTurckLieCovDerivA_backgroundSplit
+    (g₀ g₁ g_bg : SmoothRiemannianMetric I M)
+    (X P Q : Π b : M, TangentSpace I b) (x : M)
+    (hP : MDifferentiableAt I (I.prod 𝓘(ℝ, E))
+      (fun b => TotalSpace.mk' E (E := TangentSpace I) b (P b)) x)
+    (hQ : MDifferentiableAt I (I.prod 𝓘(ℝ, E))
+      (fun b => TotalSpace.mk' E (E := TangentSpace I) b (Q b)) x) :
+    deTurckLieCovDerivA (I := I) g₁ g_bg X P Q x =
+      covDerivConnDiff (I := I) g₀ g₁ X Q P x
+        - covDerivConnDiff (I := I) g₀ g_bg X Q P x
+        + PDE.DeTurck.connDiff (I := I) g₁ g₀ x
+            (PDE.DeTurck.connDiff (I := I) g₁ g_bg x (P x) (Q x)) (X x)
+        - PDE.DeTurck.connDiff (I := I) g₁ g_bg x
+            (PDE.DeTurck.connDiff (I := I) g₁ g₀ x (P x) (X x)) (Q x)
+        - PDE.DeTurck.connDiff (I := I) g₁ g_bg x (P x)
+            (PDE.DeTurck.connDiff (I := I) g₁ g₀ x (Q x) (X x)) := by
+  classical
+  have hσ := connDiff_pairing_mdiffAt (I := I) g₁ g_bg hP hQ
+  have hσB := connDiff_pairing_mdiffAt (I := I) g_bg g₀ hP hQ
+  have hsum : (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g₀ b (P b) (Q b)) =
+      (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g_bg b (P b) (Q b))
+        + (fun b : M => PDE.DeTurck.connDiff (I := I) g_bg g₀ b (P b) (Q b)) := by
+    funext b
+    change PDE.DeTurck.connDiff (I := I) g₁ g₀ b (P b) (Q b) =
+      PDE.DeTurck.connDiff (I := I) g₁ g_bg b (P b) (Q b)
+        + PDE.DeTurck.connDiff (I := I) g_bg g₀ b (P b) (Q b)
+    exact (connDiff_cocycle (I := I) g₁ g_bg g₀ b (P b) (Q b)).symm
+  have hadd := (LeviCivita (I := I) g₀).isCovariantDerivativeOnUniv.add
+    (σ := fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g_bg b (P b) (Q b))
+    (σ' := fun b : M => PDE.DeTurck.connDiff (I := I) g_bg g₀ b (P b) (Q b)) hσ hσB
+  have hsplit : (LeviCivita (I := I) g₀).toFun
+      (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g_bg b (P b) (Q b)) x (X x)
+      = (LeviCivita (I := I) g₀).toFun
+          (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g₀ b (P b) (Q b)) x (X x)
+        - (LeviCivita (I := I) g₀).toFun
+          (fun b : M => PDE.DeTurck.connDiff (I := I) g_bg g₀ b (P b) (Q b)) x (X x) := by
+    have h3 : (LeviCivita (I := I) g₀).toFun
+        (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g₀ b (P b) (Q b)) x (X x)
+        = (LeviCivita (I := I) g₀).toFun
+            (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g_bg b (P b) (Q b)) x (X x)
+          + (LeviCivita (I := I) g₀).toFun
+            (fun b : M => PDE.DeTurck.connDiff (I := I) g_bg g₀ b (P b) (Q b)) x (X x) := by
+      rw [hsum]
+      have h2 := congrArg (fun L => L (X x)) hadd
+      simpa using h2
+    rw [h3]
+    abel
+  have hout' : (LeviCivita (I := I) g₁).toFun
+      (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g_bg b (P b) (Q b)) x (X x)
+      = (LeviCivita (I := I) g₀).toFun
+          (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g_bg b (P b) (Q b)) x (X x)
+        + PDE.DeTurck.connDiff (I := I) g₁ g₀ x
+            (PDE.DeTurck.connDiff (I := I) g₁ g_bg x (P x) (Q x)) (X x) := by
+    have h : PDE.DeTurck.connDiff (I := I) g₁ g₀ x
+        (PDE.DeTurck.connDiff (I := I) g₁ g_bg x (P x) (Q x)) (X x)
+        = (LeviCivita (I := I) g₁).toFun
+            (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g_bg b (P b) (Q b)) x (X x)
+          - (LeviCivita (I := I) g₀).toFun
+            (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g_bg b (P b) (Q b)) x (X x) :=
+      PDE.DeTurck.connDiff_apply (I := I) g₁ g₀ (σ := fun b : M =>
+        PDE.DeTurck.connDiff (I := I) g₁ g_bg b (P b) (Q b)) hσ (X x)
+    rw [h]
+    abel
+  have hinnP' : (LeviCivita (I := I) g₁).toFun P x (X x)
+      = (LeviCivita (I := I) g₀).toFun P x (X x)
+        + PDE.DeTurck.connDiff (I := I) g₁ g₀ x (P x) (X x) := by
+    have h : PDE.DeTurck.connDiff (I := I) g₁ g₀ x (P x) (X x)
+        = (LeviCivita (I := I) g₁).toFun P x (X x)
+          - (LeviCivita (I := I) g₀).toFun P x (X x) :=
+      PDE.DeTurck.connDiff_apply (I := I) g₁ g₀ (σ := P) hP (X x)
+    rw [h]
+    abel
+  have hinnQ' : (LeviCivita (I := I) g₁).toFun Q x (X x)
+      = (LeviCivita (I := I) g₀).toFun Q x (X x)
+        + PDE.DeTurck.connDiff (I := I) g₁ g₀ x (Q x) (X x) := by
+    have h : PDE.DeTurck.connDiff (I := I) g₁ g₀ x (Q x) (X x)
+        = (LeviCivita (I := I) g₁).toFun Q x (X x)
+          - (LeviCivita (I := I) g₀).toFun Q x (X x) :=
+      PDE.DeTurck.connDiff_apply (I := I) g₁ g₀ (σ := Q) hQ (X x)
+    rw [h]
+    abel
+  have hsplitT2 : PDE.DeTurck.connDiff (I := I) g₁ g_bg x
+      ((LeviCivita (I := I) g₀).toFun P x (X x)
+        + PDE.DeTurck.connDiff (I := I) g₁ g₀ x (P x) (X x)) (Q x)
+      = PDE.DeTurck.connDiff (I := I) g₁ g_bg x
+          ((LeviCivita (I := I) g₀).toFun P x (X x)) (Q x)
+        + PDE.DeTurck.connDiff (I := I) g₁ g_bg x
+            (PDE.DeTurck.connDiff (I := I) g₁ g₀ x (P x) (X x)) (Q x) := by
+    rw [map_add]
+    rfl
+  have hsplitT3 : PDE.DeTurck.connDiff (I := I) g₁ g_bg x (P x)
+      ((LeviCivita (I := I) g₀).toFun Q x (X x)
+        + PDE.DeTurck.connDiff (I := I) g₁ g₀ x (Q x) (X x))
+      = PDE.DeTurck.connDiff (I := I) g₁ g_bg x (P x)
+          ((LeviCivita (I := I) g₀).toFun Q x (X x))
+        + PDE.DeTurck.connDiff (I := I) g₁ g_bg x (P x)
+            (PDE.DeTurck.connDiff (I := I) g₁ g₀ x (Q x) (X x)) :=
+    map_add _ _ _
+  have hT2c : PDE.DeTurck.connDiff (I := I) g₁ g_bg x
+      ((LeviCivita (I := I) g₀).toFun P x (X x)) (Q x)
+      = PDE.DeTurck.connDiff (I := I) g₁ g₀ x
+          ((LeviCivita (I := I) g₀).toFun P x (X x)) (Q x)
+        - PDE.DeTurck.connDiff (I := I) g_bg g₀ x
+            ((LeviCivita (I := I) g₀).toFun P x (X x)) (Q x) := by
+    have h := connDiff_cocycle (I := I) g₁ g_bg g₀ x
+      ((LeviCivita (I := I) g₀).toFun P x (X x)) (Q x)
+    rw [← h]
+    abel
+  have hT3c : PDE.DeTurck.connDiff (I := I) g₁ g_bg x (P x)
+      ((LeviCivita (I := I) g₀).toFun Q x (X x))
+      = PDE.DeTurck.connDiff (I := I) g₁ g₀ x (P x)
+          ((LeviCivita (I := I) g₀).toFun Q x (X x))
+        - PDE.DeTurck.connDiff (I := I) g_bg g₀ x (P x)
+            ((LeviCivita (I := I) g₀).toFun Q x (X x)) := by
+    have h := connDiff_cocycle (I := I) g₁ g_bg g₀ x (P x)
+      ((LeviCivita (I := I) g₀).toFun Q x (X x))
+    rw [← h]
+    abel
+  have hA : covDerivConnDiff (I := I) g₀ g₁ X Q P x
+      = (LeviCivita (I := I) g₀).toFun
+          (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g₀ b (P b) (Q b)) x (X x)
+        - PDE.DeTurck.connDiff (I := I) g₁ g₀ x (P x)
+            ((LeviCivita (I := I) g₀).toFun Q x (X x))
+        - PDE.DeTurck.connDiff (I := I) g₁ g₀ x
+            ((LeviCivita (I := I) g₀).toFun P x (X x)) (Q x) := rfl
+  have hB : covDerivConnDiff (I := I) g₀ g_bg X Q P x
+      = (LeviCivita (I := I) g₀).toFun
+          (fun b : M => PDE.DeTurck.connDiff (I := I) g_bg g₀ b (P b) (Q b)) x (X x)
+        - PDE.DeTurck.connDiff (I := I) g_bg g₀ x (P x)
+            ((LeviCivita (I := I) g₀).toFun Q x (X x))
+        - PDE.DeTurck.connDiff (I := I) g_bg g₀ x
+            ((LeviCivita (I := I) g₀).toFun P x (X x)) (Q x) := rfl
+  change (LeviCivita (I := I) g₁).toFun
+      (fun b : M => PDE.DeTurck.connDiff (I := I) g₁ g_bg b (P b) (Q b)) x (X x)
+      - PDE.DeTurck.connDiff (I := I) g₁ g_bg x
+          ((LeviCivita (I := I) g₁).toFun P x (X x)) (Q x)
+      - PDE.DeTurck.connDiff (I := I) g₁ g_bg x (P x)
+          ((LeviCivita (I := I) g₁).toFun Q x (X x)) = _
+  rw [hout', hinnP', hinnQ', hsplitT2, hsplitT3, hT2c, hT3c, hsplit, hA, hB]
+  abel
+
+set_option linter.unusedSectionVars false in
+theorem dLaCovKernel_backgroundSplit (g₀ g₁ g_bg : SmoothRiemannianMetric I M) (x : M)
+    (v0 p q : TangentSpace I x) :
+    dLaCovKernel (I := I) g₁ g_bg x v0 p q =
+      covDerivConnDiff (I := I) g₀ g₁
+          (smoothExtensionTangent (I := I) x v0)
+          (smoothExtensionTangent (I := I) x q)
+          (smoothExtensionTangent (I := I) x p) x
+        - covDerivConnDiff (I := I) g₀ g_bg
+            (smoothExtensionTangent (I := I) x v0)
+            (smoothExtensionTangent (I := I) x q)
+            (smoothExtensionTangent (I := I) x p) x
+        + PDE.DeTurck.connDiff (I := I) g₁ g₀ x
+            (PDE.DeTurck.connDiff (I := I) g₁ g_bg x p q) v0
+        - PDE.DeTurck.connDiff (I := I) g₁ g_bg x
+            (PDE.DeTurck.connDiff (I := I) g₁ g₀ x p v0) q
+        - PDE.DeTurck.connDiff (I := I) g₁ g_bg x p
+            (PDE.DeTurck.connDiff (I := I) g₁ g₀ x q v0) := by
+  rw [dLaCovKernel_apply_extend (I := I) g₁ g_bg x v0 p q]
+  rw [deTurckLieCovDerivA_backgroundSplit (I := I) g₀ g₁ g_bg
+    (smoothExtensionTangent (I := I) x v0)
+    (smoothExtensionTangent (I := I) x p)
+    (smoothExtensionTangent (I := I) x q) x
+    (smoothExtensionTangent_mdiff (I := I) x p x)
+    (smoothExtensionTangent_mdiff (I := I) x q x)]
+  simp only [smoothExtensionTangent_eq]
 
 end DifferentialGeometry.Integral.Connection
 
