e644884d14dd12be0ad3f8fd328c4808367d8675
Ziyang Qin <zq38@cornell.edu>
2026-08-11 21:59:18 -0400

    Produce uniform low-regularity H4 solutions

--- DifferentialGeometry.lean
+++ DifferentialGeometry.lean
@@ -3484,6 +3484,7 @@ import DifferentialGeometry.Geometry.Flow.RicciFlow.ShortTime.UnifLowBoundsSmall
 import DifferentialGeometry.Geometry.Flow.RicciFlow.ShortTime.UnifLowRegFatou
 import DifferentialGeometry.Geometry.Flow.RicciFlow.ShortTime.UnifLowRegH4Diss
 import DifferentialGeometry.Geometry.Flow.RicciFlow.ShortTime.UnifLowSolveH3
+import DifferentialGeometry.Geometry.Flow.RicciFlow.ShortTime.UnifLowSolveH4
 import DifferentialGeometry.Geometry.Flow.RicciFlow.ShortTime.UnifNZeroBound
 import DifferentialGeometry.Geometry.Flow.RicciFlow.ShortTime.UnifNZeroClass
 import DifferentialGeometry.Geometry.Flow.RicciFlow.ShortTime.UnifPathLower
