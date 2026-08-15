dfb0f0ed276df80daf0b2a422e55aa07910b0171
Ziyang Qin <zq38@cornell.edu>
2026-08-15 06:52:09 -0400

    Drop redundant synthesis-budget overrides in self-low arm and lowered curvature tower files

--- DifferentialGeometry/Analysis/Spectral/Tensor/CovGrad/CurvatureCoefficientDifferenceJetTower/Lowered.lean
+++ DifferentialGeometry/Analysis/Spectral/Tensor/CovGrad/CurvatureCoefficientDifferenceJetTower/Lowered.lean
@@ -3,7 +3,6 @@ import DifferentialGeometry.Geometry.Connection.TensorNabla.OperatorFieldOutputS
 
 noncomputable section
 
-set_option synthInstance.maxHeartbeats 1600000
 set_option maxHeartbeats 3200000
 
 open Bundle Manifold MeasureTheory Set Filter Tensor0SBundle
